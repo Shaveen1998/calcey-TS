097da84e8629551c79b932c777a44d337e452cbf
iamshaveenvf <shaveenfernando1@gmail.com>
2024-02-24 15:51:27 +0530

    avl

--- src/AVLTree.ts
+++ src/AVLTree.ts
@@ -7,17 +7,37 @@ class AVLTree{
         this.root = null
     }
 
-    public getHeight(node:AVLTreeNode|null):number{
+    private getHeight(node:AVLTreeNode|null):number{ //O(1)
          return node ? node.height: 0;
     }
 
-    public updateHeight(node:AVLTreeNode):void{ //O(1)
+    private updateHeight(node:AVLTreeNode):void{ //O(1)
         node.height = Math.max(this.getHeight(node.left), this.getHeight(node.right))
     }
 
-    public getBalanceFactor(node:AVLTreeNode):number{ //O(1)
+    private getBalanceFactor(node:AVLTreeNode):number{ //O(1)
         return this.getHeight(node.left) - this.getHeight(node.right)
     }
+
+
+    public insert(key:number):void{
+        this.root = this.insertData(this.root, key);
+    }
+
+    private insertData(node:AVLTreeNode|null, key:number):AVLTreeNode{
+        if(!node){
+            return new AVLTreeNode(key)
+        }
+        if(key<node.key){
+            node.left = this.insertData(node.left, key)
+
+        }else if(key>node.key){
+            node.right = this.insertData(node.right, key)
+        }
+
+        return node
+    }
+    
 }
 
 export default AVLTree
